28fe2ef535d5aef00371c7cd7145a885d1f5f2f2
SumitRaj01 <sumeet23051994@gmail.com>
2025-02-06 22:29:54 +0530

    @ViewChild decorator

--- src/app/container/search/search.component.ts
+++ src/app/container/search/search.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import { Component, ElementRef, EventEmitter, Output, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-search',
@@ -11,14 +11,16 @@ export class SearchComponent {
   //1. Create an event
   @Output() seacrhTextChanged:EventEmitter<string>=new EventEmitter<string>()
 
+  @ViewChild('searchInput') searchInputEl:ElementRef
+
   onSeacrhTextChanged(){
     this.seacrhTextChanged.emit(this.searchText)
   }
 
-  updateSearchText(inputEL: HTMLInputElement){
+  updateSearchText(){
       // this.searchText = event.target.value;
       // console.log(inputEL.value);
-      this.searchText=inputEL.value
+      this.searchText=this.searchInputEl.nativeElement.value
       this.seacrhTextChanged.emit(this.searchText)
   }
 
